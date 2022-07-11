ce63b73cd9586b310597e970cf5c93dcdb41de48
francois-chiv <francois.chiv@gmail.com>
2022-07-11 23:52:45 +0200

    add langages logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,28 +158,6 @@
     })
   }
 
-  let skillDisplay = select("#skills-display");
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "img/icons", true);
-  xhr.responseType = 'document';
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      var elements = xhr.response.getElementsByTagName("a");
-      for (x of elements) {
-        if ( x.href.match(/\.(jpe?g|png|gif)$/) ) {
-          let img = document.createElement("img");
-          img.src = x.href;
-          document.body.appendChild(img);
-        }
-      }
-    }
-    else {
-      alert('Request failed. Returned status of ' + xhr.status);
-    }
-  };
-  xhr.send();
-
-
   /**
    * Porfolio isotope and filter
    */
